d1b65be32d642af83bb37d2fb3b98718a7d8895c
Uliana.29 <Uliana.29@DESKTOP-6DD5K9D>
2024-01-18 16:41:58 +0300

    Encoding

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	[last_name]		NVARCHAR(100) NOT NULL,
+	[first_name]	NVARCHAR(100) NOT NULL,
+	[birth_day]		DATE	NOT NULL,
+)
+GO
